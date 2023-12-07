7f53e22c26377ecb5032601da6f4352bac54222b
davidd <david.ceglia.diaz@gmail.com>
2023-12-07 22:34:27 +1100

    Added a custom loader for json dumped populations using the string api

--- prompt_breeder/provider/json_file.py
+++ prompt_breeder/provider/json_file.py
@@ -3,6 +3,12 @@ import os
 import json
 import random
 from pydantic import BaseModel, ConfigDict
+from prompt_breeder.types import Population, UnitOfEvolution
+from prompt_breeder.prompts.string import (
+    StringTaskPrompt,
+    StringProblemDescription,
+    StringMutationPrompt,
+)
 
 
 class JsonListLoad(BaseModel):
@@ -41,3 +47,22 @@ class RandomJsonListLoad(JsonListLoad):
 
     def __next__(self):
         return random.choice(self.data)
+
+
+def load_string_population(fp: str) -> Population:
+    d = json.load(open(fp, "r"))
+    units = [
+        UnitOfEvolution(
+            problem_description=StringProblemDescription(
+                text=u["problem_description"]["text"]
+            ),
+            task_prompt_set=[
+                StringTaskPrompt(text=t["text"]) for t in u["task_prompt_set"]
+            ],
+            mutation_prompt=StringMutationPrompt(text=u["mutation_prompt"]["text"]),
+            elites=[StringTaskPrompt(text=t["text"]) for t in u["elites"]],
+        )
+        for u in d["members"]
+    ]
+    pop = Population(members=units, age=d["age"])
+    return pop
--- tests/provider/test_json_file.py
+++ tests/provider/test_json_file.py
@@ -1,9 +1,16 @@
 import pytest  # noqa: F401
 import json
+from prompt_breeder.types import UnitOfEvolution, Population
 from prompt_breeder.prompts.string import (
+    StringProblemDescription,
     StringTaskPrompt,
+    StringMutationPrompt,
 )
-from prompt_breeder.provider.json_file import JsonListLoad, RandomJsonListLoad
+from prompt_breeder.provider.json_file import (
+    JsonListLoad,
+    RandomJsonListLoad,
+    load_string_population,
+)
 
 data = [
     "this is data0",
@@ -42,3 +49,59 @@ def test_loads(data_file):
 
     result = [next(loader) for i in range(4)]
     print(result)
+
+
+def create_pop():
+    prompt00 = StringTaskPrompt(
+        text="one Solve the math word problem, show your workings.     "
+    )
+    prompt01 = StringTaskPrompt(text="one Solve the math word problem.      ")
+    unit0 = UnitOfEvolution(
+        problem_description=StringProblemDescription(
+            text="Solve the math word problem, giving your answer as an arabic numeral."
+        ),
+        task_prompt_set=[
+            prompt00,
+            prompt01,
+        ],
+        mutation_prompt=StringMutationPrompt(text="make the task better."),
+        elites=[],
+    )
+
+    prompt10 = StringTaskPrompt(
+        text="two Solve the math word problem, show your workings."
+    )
+    prompt11 = StringTaskPrompt(text="two Solve the math word problem.")
+    unit1 = UnitOfEvolution(
+        problem_description=StringProblemDescription(
+            text="Solve the math word problem, giving your answer as an arabic numeral."
+        ),
+        task_prompt_set=[
+            prompt10,
+            prompt11,
+        ],
+        mutation_prompt=StringMutationPrompt(text="make the task better."),
+        elites=[],
+    )
+
+    population = Population(members=[unit0, unit1], age=3)
+    return population
+
+
+def test_string_loads_pop(tmp_path_factory):
+    from prompt_breeder.evolution.callbacks.save_population import SavePopulation
+
+    pth = tmp_path_factory.mktemp("data") / "pop.json"
+    pop = create_pop()
+    saver = SavePopulation(fp=str(pth))
+    saver(pop)
+
+    loaded_pop = load_string_population(str(pth))
+
+    assert isinstance(loaded_pop, Population)
+    assert len(loaded_pop.members) == 2
+    assert loaded_pop.age == 3
+
+    assert pop.members[0] == loaded_pop.members[0]
+    assert pop.members[1] == loaded_pop.members[1]
+    assert pop == loaded_pop
